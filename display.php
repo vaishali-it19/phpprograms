51a2bae9135dfb6d19d3a38b682257e22f747e38
¨vaishali-it19¨ <vaishali.it19@bitsathy.ac.in>
2019-08-21 14:46:13 +0530

    fourthccommit

--- display.php
+++ display.php
@@ -1,14 +1,21 @@
 <?php
-for($i=1;$i<=10;$i++)
 
-if($i==5)
-{
-echo"$the num is five<br>";
+ 
+for($i=2;$i<=10;$i++)
+ {
+  for($table_counter=1;$table_counter<=10;$table_counter++)
+     {
+      printf("%d * %d = %d ",$i,$table_counter,($i*$table_counter));
+      echo "  ";
+      echo "<br> ";
+          if($table_counter==10)
+          {
+          echo "<br> ";
+          }
+     }
 }
-else
-{
-echo"$the num is $i<br>";
-}
+ 
+?>
 
+ 
 
-?> 
